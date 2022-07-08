133c74eb94d9c184966a872b31d464743397dc59
alainhenryt <henryngueko@yahoo.com>
2022-07-08 13:29:40 -0600

    second commit

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "infra101-backendtest"
-    key    = "avinash/infra101-demo/backend.tfstate"
-    region = "ca-central-1"
-    dynamodb_table = "infra101-dynamoDB"
-  }
-}
--- ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "jjtech" {
-  ami           = "ami-07d95746fec4b20a3"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "tf-state-demoEC2"
-  }
-}
